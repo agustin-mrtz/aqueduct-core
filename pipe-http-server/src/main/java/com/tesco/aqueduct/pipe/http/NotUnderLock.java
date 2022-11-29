6262ebe9802f80aff3ddaca0f37572829bbfbdf5
Agustin Martinez <agustin.martinez@sonaype.com>
2022-11-29 16:24:26 -0500

    LIFT-DEMO trigger infer example

--- pipe-http-server/src/main/java/com/tesco/aqueduct/pipe/http/NotUnderLock.java
+++ pipe-http-server/src/main/java/com/tesco/aqueduct/pipe/http/NotUnderLock.java
@@ -1,12 +1,13 @@
 package com.tesco.aqueduct.pipe.http;
 
+import java.util.Arrays;
 import java.util.concurrent.FutureTask;
 
 public class NotUnderLock {
   FutureTask<Object> future = null;
 
   public void callFutureSetOk() {
-    future.run();
+    future.set();
   }
 
   public synchronized void firstAcquisitionBad() {
@@ -17,5 +18,10 @@ public class NotUnderLock {
     synchronized (o) {
       firstAcquisitionBad();
     }
+  }
+
+  public void triggerErrorProne() {
+      String[] foo = new String[42];
+      Arrays.fill(foo, 42);
   }
 }
